811b16fad4ba0dae01141f55b9c4cff30a988e3c
Leonidas <leonidas.drawanz@gmail.com>
2022-04-18 19:01:12 -0300

    half of the first req, I was able to put the results of the api into the 'data' state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Provider from './Context/Provider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    // Observação: Para evitar erros no avaliador, na preparação do seu ambiente de trabalho para o uso do Context API, é necessário que o <Provider> seja usado dentro do componente App.js e não no arquivo index.js. Esta orientação tem finalidade exclusiva para os testes deste projeto.
+    <Provider>
+      <span>Hello, App!</span>
+    </Provider>
   );
 }
 
---
+++ src/Context/Provider.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import ContextProvider from './ContextProvider';
+
+export default function Provider({ children }) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function getPlanets() {
+      const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const { results } = await request.json();
+      setData(results);
+    }
+    getPlanets();
+  }, []);
+
+  const context = { data };
+
+  return (
+    <ContextProvider.Provider value={ context }>
+      {children}
+    </ContextProvider.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
