0d2f48586c6137974f492d44c3bb1f812f47389f
Leonidas <leonidas.drawanz@gmail.com>
2022-04-19 11:33:07 -0300

    lint corrections

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ContextProvider from '../Context/ContextProvider';
 
